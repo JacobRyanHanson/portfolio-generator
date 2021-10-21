84adcbd7d1aed0f87ab8fb9ff11594c424af9024
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-10-21 18:58:10 -0500

    Add validation

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 // Descructuring example.
 // let a, b;
-// [a,b] = [0, 1];
+// [a, b] = [0, 1];
 //------------------------
 const inquirer = require("inquirer");
 
-promptUser()
-    .then(promptProject)
-    .then(function (portfolioData) {
-        return console.log(portfolioData)
-    });
+promptUser().then(promptProject).then(function (portfolioData) {
+    return console.log(portfolioData)
+});
 
 function promptUser() {
     return inquirer.prompt([{
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
+            type: "input",
+            name: "name",
+            message: "What is your name? (Required)",
+            validate: function (nameInput) {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
         },
         {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username'
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub Username (Required)",
+            validate: function (githubInput) {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub Username!");
+                    return false;
+                }
+            }
         },
         {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself:'
+            type: "input",
+            name: "about",
+            message: "Provide some information about yourself:"
         }
     ]);
 }
@@ -39,36 +53,60 @@ Add a New Project
 =================
 `);
     return inquirer.prompt([{
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of your project?'
+            type: "input",
+            name: "name",
+            message: "What is the name of your project? (Required)",
+            validate: function (githubInput) {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the name of your project!");
+                    return false;
+                }
+            }
         },
         {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of the project (Required)'
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project (Required)",
+            validate: function (githubInput) {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description of the project!");
+                    return false;
+                }
+            }
         },
         {
-            type: 'checkbox',
-            name: 'languages',
-            message: 'What did you build this project with? (Check all that apply)',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+            type: "checkbox",
+            name: "languages",
+            message: "What did you build this project with? (Check all that apply)",
+            choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
         },
         {
-            type: 'input',
-            name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            type: "input",
+            name: "link",
+            message: "Enter the GitHub link to your project. (Required)",
+            validate: function (githubInput) {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the GitHub link to your project!");
+                    return false;
+                }
+            }
         },
         {
-            type: 'confirm',
-            name: 'feature',
-            message: 'Would you like to feature this project?',
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
             default: false
         },
         {
-            type: 'confirm',
-            name: 'confirmAddProject',
-            message: 'Would you like to enter another project?',
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project?",
             default: false
         }
     ]).then(function (projectData) {
@@ -80,14 +118,14 @@ Add a New Project
         }
     });
 };
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
 // const pageHTML = generatePage(userName, githubName);
 
-// fs.writeFile('./index.html', pageHTML, function (error) {
+// fs.writeFile("./index.html", pageHTML, function (error) {
 //     if (error) {
 //         throw new Error(error);
 //     }
-//     console.log('Portfolio complete! Check out index.html to see the output!');
+//     console.log("Portfolio complete! Check out index.html to see the output!");
 // });
