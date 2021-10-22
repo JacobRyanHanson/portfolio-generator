0f6f7a04fd0b61e4fa84c118e627d5be0e999ab1
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-10-22 12:31:08 -0500

    Add page layout logic

--- app.js
+++ app.js
@@ -4,8 +4,17 @@
 //------------------------
 const inquirer = require("inquirer");
 
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
 promptUser().then(promptProject).then(function (portfolioData) {
-    return console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, function (error) {
+        if (error) {
+            throw new Error(error);
+        }
+    });
 });
 
 function promptUser() {
@@ -45,7 +54,9 @@ function promptUser() {
             type: "input",
             name: "about",
             message: "Provide some information about yourself:",
-            when: function ({confirmAbout}) {
+            when: function ({
+                confirmAbout
+            }) {
                 if (confirmAbout) {
                     return true;
                 } else {
@@ -130,15 +141,4 @@ Add a New Project
             return portfolioData;
         }
     });
-};
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
-
-// const pageHTML = generatePage(userName, githubName);
-
-// fs.writeFile("./index.html", pageHTML, function (error) {
-//     if (error) {
-//         throw new Error(error);
-//     }
-//     console.log("Portfolio complete! Check out index.html to see the output!");
-// });
+}
